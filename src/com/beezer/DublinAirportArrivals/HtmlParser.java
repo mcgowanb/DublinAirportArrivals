69e3908f37fe4f64b377bba318fa894c72a47c6b
Brian <mcgowan.b@gmail.com>
2014-02-28 20:49:05 +0000

    no message here

--- src/com/beezer/DublinAirportArrivals/HtmlParser.java
+++ src/com/beezer/DublinAirportArrivals/HtmlParser.java
@@ -14,7 +14,7 @@ public class HtmlParser {
 	private String url;
 	private String airline;
 	public static String lastFlight;
-	private String catcherA;
+	private String charCatcher;
 	static ArrayList<ArrivalDetails> capture = new ArrayList<ArrivalDetails>();
 
 		
@@ -23,7 +23,7 @@ public class HtmlParser {
 	
 		url = config.getProperty("url");
 		airline = config.getProperty("airline");
-		catcherA = config.getProperty("stringCatcher");
+		charCatcher = config.getProperty("stringCatcher");
 		
 		print("Fetching........%s%s%s",airline," data from: ", url);
 		
@@ -32,39 +32,58 @@ public class HtmlParser {
 	public ArrayList<ArrivalDetails> process() throws IOException {
 		Document doc = Jsoup.connect(url).get(); 
 		Elements tableRow = doc.getElementsByTag("tr");
-		Element image = doc.select("img").first();
+		String[] stitcher = new String[10];
 		
 		for (Element tr : tableRow) {
 			if (tr.text().contains(airline)) {
 				
-				String terminal;
-			//	String url = image.attr("abs:src");
-			//	System.out.println("blah blah " + url);
-				
 				String scrapedText = tr.text();
 				String terminalCatcher = tr.outerHtml();
 				if(terminalCatcher.contains("images/t1.jpg")){
-					terminal = "T1";
+					stitcher[0] = "T1";
 				}
 				else if(terminalCatcher.contains("images/t2.jpg")){
-					terminal = "T2";
+					stitcher[0] = "T2";
 				}
-				else terminal = "Error";
+				else stitcher[0] = "Error";
+			
 				
-				String origin = scrapedText.substring(0,
-						scrapedText.indexOf(catcherA, 1));
-				int charCounter = origin.length();
+				String origin = scrapedText.substring(0,scrapedText.indexOf(charCatcher, 1));
+				origin = origin.trim();
+				int charCounter = origin.length()+1;
+				stitcher[1] = origin;
+				int w = origin.length();
+			//	print(" %s %s", origin, origin.length());
+				
+				scrapedText = trim(scrapedText, origin.length(),scrapedText.length());
+				
 
-				String carrier = scrapedText.substring(charCounter,
-						(airline.length() + charCounter));
-				charCounter += carrier.length();
+				String carrier = scrapedText.substring(charCounter,(airline.length() + charCounter));
+				carrier = carrier.trim();
+				charCounter += carrier.length()+1;
+				stitcher[2] = carrier;
+				int x = carrier.length();
+			//	print(" %s %s", carrier, carrier.length());
 
-				String flightNo = scrapedText.substring(charCounter,
-						(charCounter + 7));
-				charCounter += 7;
-				System.out.println(terminal +" Origin: " + origin + origin.length()
-						+ "\t\t" + carrier + carrier.length() + "\t" + flightNo
-						+ flightNo.length());
+				String flightNo = scrapedText.substring(charCounter,(charCounter + 7));
+				flightNo = flightNo.trim();
+				charCounter += flightNo.length()+1;
+				stitcher[3] = flightNo;
+				int y = flightNo.length();
+			//	print(" %s %s", flightNo, flightNo.length());
+				
+				String schedTime = scrapedText.substring(charCounter,charCounter + 12);
+				schedTime = schedTime.trim();
+				charCounter += schedTime.length()+1;
+				stitcher[4] = schedTime;
+				int z = schedTime.length();
+			//	print(" %s %s", schedTime, schedTime.length());
+				
+				String status = scrapedText.substring(charCounter);
+				stitcher[5] = status;
+			//	print(" %s %s", status, status.length());
+				
+				System.out.println(stitcher[0] +" Origin: " + origin + "\t\t" + carrier + "\t" + flightNo + "\t" + schedTime + "  " + "\t" + status);
 			}
 
 		}
@@ -80,5 +99,12 @@ public class HtmlParser {
 
 	public static void print(String msg, Object... args) {
 		System.out.println(String.format(msg, args));
+	}
+	
+	private static String trim(String s, int begin, int end) {
+			String a;
+			a = s.trim();
+			return a.substring(begin, end);
+
 	}
 }
